82fd1aa867e8ee02f2ab4458c811e08e9a99e9f9
nirajwalke <100526625+nirajwalke@users.noreply.github.com>
2022-04-16 21:21:32 +0530

    src folder

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import './Projects.css'
+import { Fade } from 'react-reveal'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import { featured_projects } from '../../data/featured_projects.json'
+import Section from '../section/Section'
+import FeaturedProject from '../featuredProject/FeaturedProject'
+
+const useStyles = makeStyles((theme) => ({
+  moreProjects: {
+    '&': {
+      margin: '20px auto',
+      backgroundColor: '#39b175',
+      boxShadow: 'none',
+      '&:hover': {
+        backgroundColor: '#0be779',
+        boxShadow: 'none',
+      },
+    },
+    '& > *': {
+      color: 'white',
+      padding: 4,
+      fontSize: '15px',
+      fontWeight: '600',
+    },
+  },
+}))
+
+const Projects = () => {
+  const classes = useStyles()
+
+  return (
+    <Section title="Projects">
+      <div className="projects-content">
+        <ul className="projects-list">
+          {featured_projects.map((featuredProject) => {
+            return (
+              <li key={`featured-project-${featuredProject.id}`}>
+                <Fade bottom duration={1000} distance="20px">
+                  <FeaturedProject
+                    name={featuredProject.name}
+                    link={featuredProject.link}
+                    description={featuredProject.description}
+                    colour={featuredProject.colour}
+                    languages={featuredProject.languages}
+                  />
+                </Fade>
+              </li>
+            )
+          })}
+        </ul>
+        <Fade bottom duration={1000} distance="20px">
+          <div className="more-projects-wrapper">
+            <a
+              className="project-link"
+              href={'https://github.com/nirajwalke'}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                className={classes.moreProjects}
+                type="button"
+                variant="contained"
+              >
+                more projects
+              </Button>
+            </a>
+          </div>
+        </Fade>
+      </div>
+    </Section>
+  )
+}
+
+export default Projects
