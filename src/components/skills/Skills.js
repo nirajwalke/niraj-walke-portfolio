ce152152fff06764775bc9acbb1f5d9e864a2966
nirajwalke <100526625+nirajwalke@users.noreply.github.com>
2022-05-16 22:58:19 +0530

    Update Skills.js

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -30,21 +30,7 @@ const Skills = () => {
                 {skills.map((skills) => {
                   return (
                     <li className="skill-bar-wrapper" key={skills.skillName}>
-                      <div
-                        className="skill-bar"
-                        style={
-                          isVisibleSkillsWrapper
-                            ? {
-                                transition: `${
-                                  1 + skills.id / 10
-                                }s width ease-in-out`,
-                                width: width * (skills.amount / 100),
-                              }
-                            : {
-                                width: 1,
-                              }
-                        }
-                      ></div>
+                      
                       <div className="skill-name">{skills.skillName}</div>
                     </li>
                   )
